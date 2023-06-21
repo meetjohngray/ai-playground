dc85ca145da3ad115f230dbb5752aabfa92d621f
John Gray <john.edwin.gray@gmail.com>
2023-06-22 10:04:40 +1200

    complete conversion to vite

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import { join } from 'node:path'
+import path from 'node:path';
 import express from 'express'
 import 'dotenv/config'
 import cors, { CorsOptions } from 'cors'
@@ -7,9 +7,15 @@ import { Configuration, OpenAIApi } from 'openai'
 const server = express()
 
 server.use(express.json())
-server.use(express.static(join(__dirname, './public')))
+server.use(express.static(path.join(__dirname, './public')))
 server.use(cors('*' as CorsOptions))
 
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static(path.resolve(__dirname, '../assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../index.html'))
+  })
+}
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
@@ -20,22 +26,20 @@ async function getCompletion() {
   try {
     const chatCompletion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [
-        {role: 'user', content: 'Hello, who am I?'},
-      ]
+      messages: [{ role: 'user', content: 'Hello, who am I?' }],
     })
     console.log(chatCompletion.data.choices[0].message)
-  }
-  catch (error: any) {
+  } catch (error: any) {
     if (error.response) {
-      console.log(error.response.status);
-      console.log(error.response.data);
+      console.log(error.response.status)
+      console.log(error.response.data)
     } else {
-      console.log(error.message);
+      console.log(error.message)
     }
   }
 }
 
 getCompletion()
+
 
 export default server
